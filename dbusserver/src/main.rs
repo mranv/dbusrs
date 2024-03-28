9704450371f3941c0b5b915637ff3c5cb286bdcd
Anubhav Gain <iamanubhavgain@gmail.com>
2024-03-28 14:26:02 +0530

    dbus-rs, dbus_tree, are both had package conflicts so will start using zbus from next commit

--- dbusserver/src/main.rs
+++ dbusserver/src/main.rs
@@ -1,40 +1,32 @@
-extern crate dbus;
+use dbus::{blocking::Connection, message::Message, strings::RequestName};
+use std::error::Error;
 
-use dbus::{Connection, BusType, NameFlag, ConnectionItem};
-use std::sync::Arc;
-use dbus::tree::{Factory, MethodErr, MTFn, MTStatic, Signal, EmitsChangedSignal};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::time::Duration;
-
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // Connect to the session bus (could also be `Connection::new_system()` for system bus)
-    let c = Connection::get_private(BusType::Session).unwrap();
+    let c = Connection::new_session()?;
 
     // Request a name on the bus
-    c.register_name("com.example.SampleService", NameFlag::ReplaceExisting as u32).unwrap();
+    c.request_name("com.example.SampleService", RequestName::ReplaceExisting, 0)?;
 
-    // Create a tree
-    let f = Factory::new_fn::<()>();
-    let t = f.tree(()).add(f.object_path("/com/example/SampleObject", ()).introspectable().add(
-        f.interface("com.example.SampleInterface", ()).add_m(
-            f.method("Ping", (), move |m| {
-                Ok(vec![m.msg.method_return().append("Pong")])
-            }).outarg::<&str,_>("response"),
-        ),
-    ));
+    // Create a new message handler
+    let handler = Box::new(|m, _| {
+        let response = Message::method_return();
+        let response = response.append1("Pong".to_string());
+        Ok(vec![response])
+    });
 
-    // Export the tree on the connection
-    t.set_registered(&c, true).unwrap();
+    // Add the message handler
+    c.register_object_path("/com/example/SampleObject")?;
+    c.register_interface(
+        "/com/example/SampleObject",
+        "com.example.SampleInterface",
+    )?;
+    c.add_method_handler("Ping", "/com/example/SampleObject", "com.example.SampleInterface", handler)?;
 
     println!("Server is listening for incoming messages...");
 
     // Event loop
     loop {
-        match c.incoming(1000).next() {
-            Some(msg) => {
-                c.process(msg);
-            }
-            None => {}
-        }
+        c.handle_incoming(None)?;
     }
 }
